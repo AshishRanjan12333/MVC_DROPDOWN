329af4956181af00cf1139d061c4481e01b088d8
MDABDULMALIK121 <abdulmalikjmi6@gmail.com>
2022-03-09 17:40:21 +0530

    Add project files.

---
+++ MVC_DROPDOWN/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace MVC_DROPDOWN.Models
+{
+    public class ApplicationDbContext:DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> Options) : base(Options) { }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<City> Cities { get; set; }
+    }
+}
---
+++ MVC_DROPDOWN/Models/State.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_DROPDOWN.Models
+{
+    public class State
+    {
+        public int Id { get; set; }
+        public string StateName { get; set; }
+        public Country Country{ get; set; }
+    }
+}
